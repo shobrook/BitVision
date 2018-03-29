45613eeb55a9daa5d8fc5fb36d916738a87444a8
Jonathan Shobrook <shobrookj@gmail.com>
2018-03-29 15:14:00 -0500

    Refactored cross_validate(); made cosmetic changes

--- src/main.py
+++ src/main.py
@@ -36,14 +36,15 @@ blockchain_data = scraper.fetch_data(os.path.dirname(os.getcwd()) + "/data/block
 
 print("Preprocessing")
 
-data = (price_data.pipe(pp.calculate_indicators)
-        .pipe(pp.merge_datasets, other_sets=[blockchain_data]) # [blockchain_data, coindesk_headlines]
-        .pipe(pp.binarize_labels)
-        .pipe(pp.fix_null_vals)
-        .pipe(pp.add_lag_variables, lag=3)
-        .pipe(pp.power_transform)
-        .pipe(pp.select_features, method="RecursiveFE", skip=SELECT_FEATURES)
-        )
+data = (
+    price_data.pipe(pp.calculate_indicators)
+    .pipe(pp.merge_datasets, other_sets=[blockchain_data]) # [blockchain_data, coindesk_headlines]
+    .pipe(pp.binarize_labels)
+    .pipe(pp.fix_null_vals)
+    .pipe(pp.add_lag_variables, lag=3)
+    .pipe(pp.power_transform)
+    .pipe(pp.select_features, method="RecursiveFE", skip=SELECT_FEATURES)
+    )
 x_train, x_test, y_train, y_test = pp.split(data, test_size=.2, balanced=True)
 
 
@@ -90,16 +91,31 @@ print("Evaluating")
 print("\tLogistic Regression Estimator")
 log_reg.plot_cnf_matrix()
 log_reg.cross_validate(method="holdout")
-log_reg.cross_validate(method="RollingWindow", data=data)
+log_reg.cross_validate(
+    method="RollingWindow",
+    data=data,
+    window_size=.9,
+    test_size=.1
+    )
 
 # Random Forest
 print("\tRandom Forest Classifier")
 rand_forest.plot_cnf_matrix()
 rand_forest.cross_validate(method="holdout")
-rand_forest.cross_validate(method="RollingWindow", data=data)
+rand_forest.cross_validate(
+    method="RollingWindow",
+    data=data,
+    window_size=.9,
+    test_size=.1
+    )
 
 # Gradient Boosting
 print("\tGradient Boosting Classifier")
 grad_boost.plot_cnf_matrix()
 grad_boost.cross_validate(method="holdout")
-grad_boost.cross_validate(method="RollingWindow", data=data)
+grad_boost.cross_validate(
+    method="RollingWindow",
+    data=data,
+    window_size=.9,
+    test_size=.1
+    )
