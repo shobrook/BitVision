bb0ab5f8de39a6c86f43d02d0500b41d393cd4d5
Jonathan Shobrook <shobrookj@gmail.com>
2018-06-11 23:55:36 -0400

    Started on the Model object

--- bitvision/engine/model.py
+++ bitvision/engine/model.py
@@ -1,1 +1,42 @@
-# TODO
+#########
+# GLOBALS
+#########
+
+
+import os
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.externals import joblib
+
+#PARENT_DIR = os.path.dirname(os.getcwd())
+
+
+#########
+# HELPERS
+#########
+
+
+######
+# MAIN
+######
+
+
+class Model(object):
+	def __init__(self, training_data, hyperparameterize=False):
+        self.x_train, self.y_train = self.__split(training_data, balanced=True)
+
+        self.scaler = StandardScaler()
+		self.scaler.fit(training_data)
+
+        self.model = RandomForestClassifier(n_estimators=500)
+        self.model.fit(self.scaler.transform(self.x_train), self.y_train)
+
+        joblib.dump(self.model, "../cache/model.pkl")
+
+
+    def __split(self, df, balanced):
+        pass
+
+
+    def predict(self, vector):
+        return self.model.predict(self.scaler.transform(vector))
