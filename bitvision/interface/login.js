03c9c0400b8b9e12ebe5bbc848da57b0f7ac27ef
alichtman <aaronlichtman@gmail.com>
2018-07-17 01:41:55 -0400

    Check for yaml file and fix async bug

--- bitvision/interface/login.js
+++ bitvision/interface/login.js
@@ -5,24 +5,62 @@
 let figlet = require('figlet')
 let chalk = require('chalk')
 let inquirer = require('inquirer')
+let fs = require('fs')
+let yaml = require('js-yaml')
+
+const dotfilePath = '~/.bitvision'
 
 // TODO: If the ".bitvision" file doesn't exist, create it and run the login flow.
 // If it does, authenticate and run the CLI.
 
-figlet.text('Bitvision', {
-  font: 'Univers',
-  horizontalLayout: 'default',
-  verticalLayout: 'default'
-}, function(err, data) {
-  if (err) {
-    console.log('Something went wrong...');
-    console.dir(err);
-    return;
+// TODO: Support Windows.
+
+/**
+ * Returns true if dotfile exists, false otherwise.
+ *
+ * @return {Boolean} File exists.
+ */
+ function checkForDotFile() {
+  fs.stat(dotfilePath, function(err, stat) {
+    if (err == null) {
+      return true
+    } else if (err.code == 'ENOENT') {
+      return false
+    }
+  });
+}
+
+function readLoginInfo() {
+  try {
+    let config = yaml.safeLoad(fs.readFileSync(dotfilePath, 'utf8'));
+    let indentedJson = JSON.stringify(config, null, 4);
+    console.log(indentedJson);
+
+    let key = config.credentials.key
+    let secret = config.credentials.secret
+    let passphrase = config.credentials.passphrase
+  } catch (e) {
+    console.log(e);
   }
-  console.log(chalk.blue.bold(data));
-  console.log(chalk.red.bold("Written by Jon Shobrook"), chalk.blue("(https://github.com/shobrook)"), chalk.red.bold("and Aaron Lichtman"), chalk.blue("(https://github.com/alichtman)"))
-  console.log(chalk.red.bold("Source code:"), chalk.blue("https://github.com/shobrook/BitVision"), "\n")
-});
+}
+
+function printSplashScreen(callback) {
+  figlet.text('Bitvision', {
+    font: 'Univers',
+    horizontalLayout: 'default',
+    verticalLayout: 'default'
+  }, function(err, data) {
+    if (err) {
+      console.log('Something went wrong...');
+      console.dir(err);
+      return;
+    }
+    console.log(chalk.blue.bold(data));
+    console.log(chalk.red.bold("Written by Jon Shobrook"), chalk.blue("(https://github.com/shobrook)"), chalk.red.bold("and Aaron Lichtman"), chalk.blue("(https://github.com/alichtman)"))
+    console.log(chalk.red.bold("Source code:"), chalk.blue("https://github.com/shobrook/BitVision"), "\n")
+    callback()
+  })
+}
 
 // Authentication prompt
 
@@ -33,10 +71,8 @@ function validateInput(value) {
   return 'You must enter some value here.';
 }
 
-// TODO: Figure out how to make this prompt after the splash screen has finished printing. I know that sleep is not the right answer.
-// JS can fuck right off zkfglkdjffladksfjlask
-
-inquirer.prompt([
+function getLoginInfo() {
+  inquirer.prompt([
   {
     type: 'input',
     name: 'key',
@@ -61,7 +97,20 @@ inquirer.prompt([
       return validateInput(value)
     }
   }
-]).then(answers => {
+  ]).then(answers => {
   // TODO: Store answers["key"], answers["secret"], answers["passphrase"] in "~/.BitVision"
   console.log(answers)
 });
+}
+
+// TODO: WHY DOES THIS RETURN UNDEFINED AKSDJFALKJDFALKD
+var x = checkForDotFile()
+console.log(x)
+
+// If config exists, log in and start CLI. Otherwise, show onboarding.
+if (checkForDotFile()) {
+  // TODO
+} else {
+  printSplashScreen(getLoginInfo)
+}
+
