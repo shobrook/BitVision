a87ab6ae5d61f739e7edcb1658726dd44428d7c2
alichtman <aaronlichtman@gmail.com>
2018-08-13 15:18:39 -0500

    Started bitstamp integration

---
+++ bitvision/services/bitstamp-trading.py
@@ -0,0 +1,61 @@
+import bitstamp.client
+import json
+from os.path import expanduser
+from pathlib import Path
+from pprint import pprint
+
+# Set Bitstamp client as Public until auth.
+bitstamp_client = bitstamp.client.Public()
+
+########
+# Config Utilities
+########
+
+
+def getConfig():
+	"""
+	Returns config file contents in JSON format.
+	"""
+	config_path = os.path.join(expanduser("~"), ".shallow-backup")
+	config_contents = Path(file_path).read_text()
+	config = json.loads(config_contents)
+	return config
+
+
+def getCredentials():
+	config = getConfig()
+	return config["credentials"]
+
+########
+# BitStamp Methods
+########
+
+
+def tradingAccountLogin():
+	"""
+	Replaces bitstamp public client with authenticated private client.
+	"""
+	creds = getCredentials()
+	bitstamp_client = bitstamp.client.Trading(username=creds["username"],
+	                                          key=creds["key"],
+	                                          secret=creds["secret"])
+
+def getAccountBalance():
+	pprint(bitstamp_client.account_balance())
+	return
+
+
+
+def main():
+	print("Ticker")
+	pprint(bitstamp_client.ticker())
+	print("Ticker Hourly")
+	pprint(bitstamp_client.ticker_hour())
+	print("Order Book")
+	pprint(bitstamp_client.order_book())
+	print("Transactions")
+	pprint(bitstamp_client.transactions())
+	print("Trading Pairs Info")
+	pprint(bitstamp_client.trading_pairs_info())
+
+
