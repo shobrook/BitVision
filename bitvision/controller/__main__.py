ab14866515aa492e506a60bc62941040f79f2eed
Jonathan Shobrook <shobrookj@gmail.com>
2018-08-28 18:48:41 -0500

    Added dummy data for get_portfolio_stats

--- bitvision/controller/__main__.py
+++ bitvision/controller/__main__.py
@@ -2,36 +2,42 @@ import sys
 from monitor import refresh
 from trader import train_and_predict, make_trade
 from bitstamp import Trading
+from crontab import CronTab
 
 
 def action(name):
-    client = Trading(
-        username="test",
-        key="test",
-        secret="test"
-    )
+    # client = Trading(
+    #     username="test",
+    #     key="test",
+    #     secret="test"
+    # )
 
     if name == "monitor_network":  # Updates price and blockchain data
         refresh([
             "price_data",
-            "indicators",
-            "blockchain"
+            "tech_indicators",
+            "blockchain_data"
         ])
     elif name == "monitor_opinions":  # Updates coindesk data
         refresh(["coindesk_stats"])
-    elif name == "monitor_performance":  # Updates portfolio data
-        refresh(["performance"])
+    elif name == "monitor_portfolio":  # Updates portfolio data
+        refresh(["portfolio_stats"])
     elif name == "toggle_algo":  # Toggles algorithmic trading
-        return
+        cron = CronTab(user=True)
+        job = cron.new(command="python3 __main__.py make_algotrade")
+        job.hour.every(24)
+
+        cron.write()
     elif name == "make_algotrade":  # Makes a scheduled trade
+        balance = client.account_balance()["usd_available"]
         # TODO: Figure out amount w/ the Kelly Criterion
         make_trade({"type": train_and_predict(), "amount": 0})
     elif name == "make_trade":  # Makes a user-defined trade
         make_trade(dict(sys.argv[2]))
     elif name == "withdraw":  # Withdraws money from user account
         client.bitcoin_withdrawal(int(sys.argv[2]), sys.argv[3])
-    elif name == "deposit_address":  # Gets wallet address for depositing BTC
-        client.bitcoin_deposit_address()  # TODO: Write this to account.json
+    # elif name == "deposit_address":  # Gets wallet address for depositing BTC
+    #     client.bitcoin_deposit_address()  # TODO: Write this to account.json
 
 
 if __name__ == "__main__":
