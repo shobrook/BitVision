b254211e89fced7dc33c99cfdf090c7b15bafc53
Jonathan Shobrook <shobrookj@gmail.com>
2018-11-13 13:34:49 -0600

    Minor refactor

--- bitvision/controller/__main__.py
+++ bitvision/controller/__main__.py
@@ -1,24 +1,36 @@
+# TODO: Rename "controller" to "services"
+
+#########
+# GLOBALS
+#########
+
+
 import sys
 import json
 from datetime import datetime, timedelta
 from crontab import CronTab
 
-from monitor import refresh
-from trader import train_and_predict, make_trade
-from bitstamp import Trading
+# Local
+from retriever import retrieve
+from trader import make_prediction, make_trade, TradingClient
+
+
+######
+# MAIN
+######
 
 
 def action(name):
     with open("./cache/config.json") as config:
         config_dict = json.load(config)
         credentials = config_dict["credentials"]
-        client = Trading(
+        client = TradingClient(
             username=credentials["username"],
             key=credentials["key"],
             secret=credentials["secret"]
         )
 
-        if name == "authenticate":  # Authenticates Bitstamp credentials
+        if name == "authenticate": # Authenticates Bitstamp credentials
             with open("./cache/config.json", 'w') as new_config:
                 try:
                     client.account_balance()
@@ -28,44 +40,60 @@ def action(name):
 
                 config_dict["logged_in"] = logged_in
                 new_config.write(json.dumps(config_dict))
-        elif name == "monitor_price":  # Updates ticker and graph data
-            refresh(["price_data"])
-        elif name == "monitor_network":  # Updates technical indicators and blockchain data
-            refresh(["tech_indicators", "blockchain_data"])
-        elif name == "monitor_opinions":  # Updates coindesk data
-            refresh(["coindesk_stats"])
-        elif name == "monitor_portfolio":  # Updates portfolio data
-            refresh(["portfolio_stats", "transactions"], client)
+        elif name == "retrieve_price_data": # Updates ticker and graph data
+            retrieve(["price_data"])
+        elif name == "retrieve_network_data": # Updates technical indicators and blockchain data
+            retrieve(["tech_indicators", "blockchain_data"])
+        elif name == "retrieve_headline_data": # Updates coindesk data
+            retrieve(["coindesk_headlines"])
+        elif name == "retrieve_portfolio_stats": # Updates portfolio data
+            retrieve(["portfolio_stats", "transaction_log"], client)
         elif name == "toggle_algo":  # Toggles algorithmic trading
             cron = CronTab(user=True)
             job_killed = False
 
             for job in cron:
-                if job.comment == "bitvision_algotrading_job":  # Job is already running, kill it
+                # Job is already running, kill it
+                if job.comment == "bitvision_algotrading_job":
                     cron.remove(job)
                     cron.write()
 
                     job_killed = True
-                    config.write(json.dumps(
-                        {**config_dict, **{"autotrade": {"next-trade-timestamp-UTC": -1, "enabled": False}}}))
+                    config.write(json.dumps({
+                        **config_dict,
+                        **{"autotrade": {
+                            "next-trade-timestamp-UTC": -1,
+                            "enabled": False
+                        }}
+                    }))
 
                     break
 
-            if not job_killed:  # Job doesn't exist, create it
-                job = cron.new(command="python3 __main__.py make_algotrade",
-                               comment="bitvision_algotrading_job")
+            if not job_killed: # Job doesn't exist, create it
+                job = cron.new(
+                    command="python3 __main__.py make_algotrade",
+                    comment="bitvision_algotrading_job"
+                )
                 job.hour.every(23)
-
                 cron.write()
-                config.write(json.dumps({**config_dict, **{"autotrade": {"next-trade-timestamp-UTC": str(
-                    datetime.utcnow() + timedelta(hours=23)), "enabled": False}}}))
-        elif name == "make_algotrade":  # Makes a scheduled trade
+
+                next_trade_time = str(datetime.utcnow() + timedelta(hours=23))
+                config.write(json.dumps({
+                    **config_dict,
+                    **{"autotrade": {
+                        "next-trade-timestamp-UTC": next_trade_time,
+                        "enabled": True
+                    }}
+                }))
+        elif name == "make_algotrade": # Makes a scheduled trade
             balance = client.account_balance()["usd_available"]
+
             # TODO: Figure out amount w/ the Kelly Criterion
-            make_trade(client, {"type": train_and_predict(), "amount": 0})
-        elif name == "make_trade":  # Makes a user-defined trade
+
+            make_trade(client, {"type": make_prediction(), "amount": 0})
+        elif name == "make_trade": # Makes a user-defined trade
             make_trade(client, dict(sys.argv[2]))
-        elif name == "withdraw":  # Withdraws money from user account
+        elif name == "withdraw": # Withdraws money from user account
             client.bitcoin_withdrawal(int(sys.argv[2]), sys.argv[3])
 
 
