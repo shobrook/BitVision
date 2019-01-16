6ed214cc0f7632e8829670b6dd6f19e16d900ccb
Jonathan Shobrook <shobrookj@gmail.com>
2019-01-16 00:17:35 -0600

    Turned error modal into generic notification modal

---
+++ modals/notification.js
@@ -0,0 +1,78 @@
+// GLOBALS //
+
+const blessed = require("blessed");
+const { colorScheme } = require("../constants");
+
+// MAIN //
+
+module.exports.createNotificationModal = (
+  screen,
+  displayText,
+  showButton = true,
+  callback = modal => {}
+) => {
+  let notification = null;
+  notification = blessed.form({
+    parent: screen,
+    keys: true,
+    type: "overlay",
+    top: "center",
+    left: "center",
+    width: 45,
+    height: 8,
+    bg: colorScheme.background,
+    color: "white"
+  });
+
+  let label = blessed.box({
+    parent: notification,
+    top: 1,
+    left: "center",
+    width: 6,
+    height: 1,
+    content: displayText.label,
+    style: { bg: colorScheme.background, fg: "white", bold: true },
+    tags: true
+  });
+
+  let hint = blessed.box({
+    parent: notification,
+    top: 2,
+    left: "center",
+    width: 38,
+    height: 3,
+    shrink: true,
+    content: displayText.hint,
+    style: { bg: colorScheme.background, fg: "black" },
+    tags: true
+  });
+
+  // Buttons
+  if (showButton) {
+    let okayButton = blessed.button({
+      parent: notification,
+      mouse: true,
+      keys: true,
+      shrink: true,
+      right: 5,
+      bottom: 1,
+      padding: { left: 4, right: 4, top: 1, bottom: 1 },
+      name: "okay",
+      content: "okay",
+      style: {
+        bg: colorScheme.confirmLight,
+        fg: "black",
+        focus: { bg: colorScheme.confirmDark, fg: "black" },
+        hover: { bg: colorScheme.confirmDark, fg: "black" }
+      }
+    });
+
+    okayButton.on("press", () => notification.destroy());
+    screen.key(["q", "Q"], (ch, key) => notification.destroy());
+
+    okayButton.focus();
+  }
+
+  screen.render();
+  callback(notification);
+};
